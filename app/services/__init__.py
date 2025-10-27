c65f0629bee8442bd506a40856a365327bef47da
Jucelio Quentino <vitorquentino@gmail.com>
2025-10-27 09:54:04 -0300

    Refactor add_files_to_index to return added files directly from add_documents method in VectorIndexManager

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -121,12 +121,11 @@ async def continue_cv_workflow(
 
 async def add_files_to_index(file_paths: list[str | Path]) -> list[str]:
     index_manager = VectorIndexManager()
-    await index_manager.add_documents(file_paths)
-    added_files = await index_manager.get_added_files()
-    return list(added_files)
+    added_files = await index_manager.add_documents(file_paths)
+    return added_files
 
 
 async def get_files_in_index() -> list[str]:
     index_manager = VectorIndexManager()
-    added_files = await index_manager.get_added_files()
-    return list(added_files)
+    all_files = await index_manager.get_added_files()
+    return all_files
